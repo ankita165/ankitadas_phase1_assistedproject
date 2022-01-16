48163cff74e52a98b1d74f1dc1546588a8abe310
ankita165 <97821476+ankita165@users.noreply.github.com>
2022-01-16 13:09:58 +0530

    methodoverloading.java

---
+++ overloadMethod.java
@@ -0,0 +1,21 @@
+package accessModifiers;
+
+public class overloadMethod {
+	
+public void area(int b,int h)
+    {
+         System.out.println("Area of Triangle : "+(0.5*b*h));
+    }
+    public void area(int r) 
+    {
+         System.out.println("Area of Circle : "+(3.14*r*r));
+    }
+
+    public static void main(String args[])
+   {
+
+overloadMethod ob=new overloadMethod();
+       ob.area(14,16);
+       ob.area(8);  
+   }
+}
