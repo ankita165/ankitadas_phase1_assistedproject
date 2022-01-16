d16b912afbbbc6cce973d7c34904c8320b619cd2
ankita165 <97821476+ankita165@users.noreply.github.com>
2022-01-16 12:48:26 +0530

    collectionassisted.java

---
+++ collection.java
@@ -0,0 +1,54 @@
+package accessModifiers;
+
+import java.util.*;
+
+public class collectionAssisted {
+
+	public static void main(String[] args) {
+		//creating arraylist
+		System.out.println("ArrayList");
+		ArrayList<String> city=new ArrayList<String>();   
+	      city.add("chennai");//
+	      city.add("mumbai");    	   
+	      System.out.println(city);  
+		
+		//creating vector
+	      System.out.println("\n");
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector();
+	      vec.addElement(15); 
+	      vec.addElement(30); 
+	      System.out.println(vec);
+		
+		//creating linkedlist
+	      System.out.println("\n");
+	      System.out.println("LinkedList");
+	      LinkedList<String> names=new LinkedList<String>();  
+	      names.add("aman");  
+	      names.add("john");  	      
+	      Iterator<String> itr=names.iterator();  
+	      while(itr.hasNext()){  
+	       System.out.println(itr.next());  
+	       
+	       //creating hashset
+	       System.out.println("\n");
+	       System.out.println("HashSet");
+	       HashSet<Integer> set=new HashSet<Integer>();  
+	       set.add(520);  
+	       set.add(521);  
+	       set.add(528);
+	       set.add(530);
+	       System.out.println(set);
+	       
+	       //creating linkedhashset
+	       System.out.println("\n");
+	       System.out.println("LinkedHashSet");
+	       LinkedHashSet<Integer> set2=new LinkedHashSet<Integer>();  
+	       set2.add(10);  
+	       set2.add(11);  
+	       set2.add(12);
+	       set2.add(13);	       
+	       System.out.println(set2);
+	      	} 
+	      }  
+	}
