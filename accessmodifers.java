df528077588f6179d3f536df4eb2d22b4438d09b
ankita165 <97821476+ankita165@users.noreply.github.com>
2022-01-16 12:33:34 +0530

    accessmodifiers.java

---
+++ accessmodifers.java
@@ -0,0 +1,25 @@
+package accessModifiers;
+
+ public class accessmodifers {
+ public void meth1() {
+ System.out.println("public access modifiers implemt");
+ }
+ private void meth2() {
+ System.out.println("private access modifiers implemt");
+ }
+void meth3() {
+ System.out.println("default access modifiers implement");
+ }
+ protected void meth4() {
+ System.out.println("protected access modifiers implement");
+ }
+ public static void main(String[] args) {
+
+ accessmodifers mod=new accessmodifers();
+ mod.meth1() ;
+ mod.meth2();
+ mod.meth3();
+ mod.meth4();
+
+}
+ }
